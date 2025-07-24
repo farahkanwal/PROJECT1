8bb69552f4e0297518e6f1c02ba1ff949527e2b9
farahkanwal <farahkanwal718@gmail.com>
2025-07-24 08:12:53 +0500

    Add files via upload

---
+++ farah.html/PROJECT/syllabusm.js
@@ -0,0 +1,79 @@
+
+
+/*let a=10;
+while(a<20){
+    console.log(a)
+    a++;
+}
+    let a="mama";
+    console.log(a);
+
+for (let i=1; i<11;i++){
+    let mul=2
+    console.log(mul)
+}
+let b=5;
+do{
+    console.log(b)
+    b++;
+}
+    while(b<10)
+        let student=["saba","anam","abu","amjad"]
+    console.log(student)
+    student.push("farah")
+    console.log(student)
+    student.unshift("abudhabi")
+    console.log(student);
+    student.pop()
+    console.log(student);
+    student.shift()
+    console.log(student);*/
+   /* let a=10;
+    if (a>25){
+        console.log("welcome")
+    }
+    else {
+        console.log("khudqahafiz")
+    }*/
+//    let a=12;
+//    let b=5;
+//    let sub=a%b;
+//    console.log(sub);
+    
+//    let c=12;
+//    let d=5;
+//    let exp=a**b;
+//    console.log(exp);
+//    var no1=2;
+//    for(i=0;no1<10;no1++){
+//     console.log("aptech")
+//    }
+//    var no=100;
+//    while(no>0){
+//     console.log(no);
+//     no--
+//    }
+//    let word1=4;
+//    let word2=4;
+//    let word=word1==word2;
+//    console.log(word);
+//    let words1=4;
+//    let words2="4";
+//    let words=("word1" && "word2");
+//    console.log(words);
+  let num=prompt("enter a number");
+  if(num%5===0){
+    console.log(num,"is a multiple of 5");
+    
+
+    }else{
+        console.log(num,"is not a multiple of 5");
+    }
+    let num1=prompt("enter a number");
+    if(num1%2===0){
+      console.log(num1,"is an even number");
+      
+  
+      }else{
+          console.log(num1,"is a odd number");
+      }
